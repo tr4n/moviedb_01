34ce6dd1c86108e2cc94d9d750bed62e844fbc23
hunguet123 <hunguet123@gmail.com>
2022-11-30 11:11:47 +0700

    create bottom navigation bar

--- app/src/main/java/com/tr4n/moviedb/MainActivity.kt
+++ app/src/main/java/com/tr4n/moviedb/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.tr4n.moviedb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, HomeActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2000)
     }
 }
---
+++ app/src/main/java/com/tr4n/moviedb/di/ViewModelModule.kt
@@ -0,0 +1,19 @@
+package com.tr4n.moviedb.di
+
+import com.tr4n.moviedb.ui.favorite.FavoriteViewModel
+import com.tr4n.moviedb.ui.home.HomeViewModel
+import com.tr4n.moviedb.ui.search.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel {
+        SearchViewModel()
+    }
+    viewModel {
+        FavoriteViewModel()
+    }
+    viewModel {
+        HomeViewModel()
+    }
+}
