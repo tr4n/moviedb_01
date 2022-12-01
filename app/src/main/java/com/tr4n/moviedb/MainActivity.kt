0ebe036f0b8a983caa0ca1a985f04059695ef558
hunguet123 <hunguet123@gmail.com>
2022-12-01 15:20:43 +0700

    fix review

--- app/src/main/java/com/tr4n/moviedb/MainActivity.kt
+++ app/src/main/java/com/tr4n/moviedb/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.tr4n.moviedb
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.tr4n.moviedb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val mainIntent = Intent(this, HomeActivity::class.java)
-            startActivity(mainIntent)
-            finish()
-        }, 2000)
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_search, R.id.navigation_favorite
+//            )
+//        )
+        //supportActionBar?.hide()
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/tr4n/moviedb/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.tr4n.moviedb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 2000)
+    }
+}
