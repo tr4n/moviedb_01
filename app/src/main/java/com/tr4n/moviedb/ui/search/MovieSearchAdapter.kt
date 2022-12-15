7442fdbc28229440b9815987c71b1b96581f5fb9
hunguet123 <hunguet123@gmail.com>
2022-12-15 23:03:27 +0700

    #6. [Search] [List movie] Endless scrolling

---
+++ app/src/main/java/com/tr4n/moviedb/ui/search/MovieSearchAdapter.kt
@@ -0,0 +1,56 @@
+package com.tr4n.moviedb.ui.search
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tr4n.moviedb.R
+import com.tr4n.moviedb.data.model.Movie
+import com.tr4n.moviedb.utils.Constant
+import kotlinx.android.synthetic.main.item_movie_search_recyclerview.view.*
+
+class MovieSearchAdapter : ListAdapter<Movie, MovieSearchAdapter.MovieSearchViewHolder>(Movie.diffCallback) {
+
+    var onItemSelected: (Long) -> Unit = {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieSearchViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_movie_search_recyclerview, parent, false)
+        return MovieSearchViewHolder(itemView, onItemSelected)
+    }
+
+    override fun onBindViewHolder(holder: MovieSearchViewHolder, position: Int) {
+        holder.binData(getItem(position))
+    }
+
+    class MovieSearchViewHolder(
+        itemView: View,
+        onItemSelected: (Long) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+                itemData?.let { movie ->
+                    onItemSelected(movie.id)
+                }
+            }
+        }
+
+        private var itemData: Movie? = null
+        fun binData(movie: Movie) {
+            itemData = movie
+            itemView.run {
+                Glide.with(this)
+                    .load(Constant.BASE_URL_IMAGE + itemData?.url)
+                    .into(imageViewMovieSearch)
+                textTitleNameMovieSearch.text = itemData?.title
+                textVoteAverage.text = itemData?.votAverage.toString()
+                textGenre.text = itemData?.genreIds.toString()
+                textReleaseYear.text = itemData?.releaseDate
+            }
+        }
+
+    }
+
+}
--- app/src/main/java/com/tr4n/moviedb/ui/search/SearchFragment.kt
+++ app/src/main/java/com/tr4n/moviedb/ui/search/SearchFragment.kt
@@ -1,25 +1,70 @@
 package com.tr4n.moviedb.ui.search
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tr4n.moviedb.R
 import com.tr4n.moviedb.base.BaseFragment
+import com.tr4n.moviedb.ui.detail.MovieDetailFragment
+import com.tr4n.moviedb.ui.home.HomeTab
+import com.tr4n.moviedb.ui.home.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_search.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : BaseFragment(R.layout.fragment_search) {
-    private val viewModel : SearchViewModel by viewModel()
+    private val viewModel : HomeViewModel by viewModel()
+    private val movieSearchAdapter = MovieSearchAdapter()
+    private var currentPage = 1
+    private var isLoading = false
 
     override fun initData() {
-        //TODO("Not yet implemented")
+        viewModel.getTabMovie(HomeTab.NOW_PLAYING.tabNameRes, currentPage)
+        movieSearchAdapter.submitList(viewModel.listMoviesNowPlaying.value)
     }
 
     override fun listenEvents() {
-        //TODO("Not yet implemented")
+        recyclerViewMovieSearch.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val visibleItemCount = recyclerView.layoutManager?.childCount
+                val total = movieSearchAdapter.itemCount
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val pastVisibleItem = layoutManager.findFirstCompletelyVisibleItemPosition()
+                if (dy > 0) {
+                    if (visibleItemCount != null && !isLoading && (visibleItemCount + pastVisibleItem) >= total) {
+                        isLoading = true
+                        viewModel.getNextTabMovie(HomeTab.NOW_PLAYING.tabNameRes,
+                            currentPage)
+                        currentPage++
+                    }
+                } else if (currentPage > 1 && dy < 0) {
+                    if (pastVisibleItem <= visibleItemCount!! && !isLoading) {
+                        isLoading = true
+                        viewModel.getPreTabMovie(HomeTab.NOW_PLAYING.tabNameRes,
+                            currentPage)
+                        currentPage--
+                    }
+                }
+            }
+        })
+        movieSearchAdapter.onItemSelected = { movieId ->
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, MovieDetailFragment.newInstance(movieId))
+                ?.addToBackStack(null)
+                ?.commit()
+        }
     }
 
     override fun observeData() {
-        //TODO("Not yet implemented")
+        viewModel.listMoviesNowPlaying.observe(viewLifecycleOwner) {
+            isLoading = false
+            if (it != null) {
+                //progressNewLyMovie.isVisible = it.isEmpty()
+            }
+            movieSearchAdapter.submitList(it)
+        }
     }
 
     override fun setupViews() {
-        //TODO("Not yet implemented")
+        recyclerViewMovieSearch.adapter = movieSearchAdapter
     }
 }
